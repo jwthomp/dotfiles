6a3362949c4bdb4ad493466452bda25c8ee7f2b9
Jeff Thompson <mythrndr@gmail.com>
2026-01-31 15:46:08 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,21 +4,29 @@ HOSTNAME=$(hostname -s)
 OS=$(uname)
 
 package_list=(
+  "aerospace"
+  "aliases"
+  "git"
+  "jj"
+  "shell"
+  "ssh"
+  "vim"
+  "wezterm"
   "zsh"
 )
 
-for package_name in "$package_list"; do
-  echo "Installing zsh files for OS: ${OS} and HOST: ${HOSTNAME}"
+for package_name in ${package_list[@]}; do
+  printf "Installing %-15s files for OS: %-10s and HOST: %s\n" ${package_name} ${OS} ${HOSTNAME}
 
   if [[ -d "common/${package_name}" ]]; then
-    stow -d common  -t ~/ ${package_name}
+    stow -d common  -t ~/ -R ${package_name}
   fi
 
   if [[ -d "os/${OS}/${package_name}" ]]; then
-    stow -d os/${OS} -t ~/ ${package_name}
+    stow -d os/${OS} -t ~/ -R ${package_name}
   fi
 
   if [[ -d "host/${HOSTNAME}/${package_name}" ]]; then
-    stow -d host/${HOSTNAME} -t ~/ ${package_name}
+    stow -d host/${HOSTNAME} -t ~/ -R ${package_name}
   fi
 done
